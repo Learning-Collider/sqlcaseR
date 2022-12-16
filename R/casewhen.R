8a43ab59962bbe4add17c2a9d605182e851d3d1a
Leoson Hoay <leosonhoay@gmail.com>
2022-12-16 12:15:11 -0600

    0.1.0

---
+++ R/casewhen.R
@@ -0,0 +1,22 @@
+#' Generate a SQL CASE WHEN statement from a mapping file
+#'
+#' This function constructs a CASE WHEN statement from a mapping file. 
+#' It assumes that the first column of the mapping file contains the original
+#' CASE WHEN values, and the second column contains the THEN values (the values
+#' to be mapped to.)
+#'
+#' @param inputfile Path to the mapping file
+#' @return A string that represents the constructed CASE WHEN statement
+#' @export
+casewhen <- function(inputfile){
+  mapping <- read.csv(inputfile)
+  statement <- "\nCASE"
+  for (i in 1:nrow(mapping)){
+    statement = paste(statement, " WHEN ", "'", mapping[i, 1],
+                      "'", " THEN ",
+                      "'", mapping[i, 2], "'", "\n", sep="")
+  }
+  cat(statement)
+  return(statement)
+}
+
