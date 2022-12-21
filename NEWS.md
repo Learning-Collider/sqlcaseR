705f7273a12b3908cc6e6ead581a94341b98bca2
Leoson Hoay <leosonhoay@gmail.com>
2022-12-21 16:44:03 -0600

    v.0.1.1

---
+++ NEWS.md
@@ -0,0 +1,11 @@
+# *News*
+
+# sqlcaseR 0.1.1 (2022-12-20)
+
+* Added functionality to read in dataframes in addition to file paths.
+
+# sqlcaseR 0.1.0 (2022-12-16)
+
+## Initial Release to GitHub
+
+* Initial Release to GitHub
--- R/casewhen.R
+++ R/casewhen.R
@@ -1,15 +1,19 @@
 #' Generate a SQL CASE WHEN statement from a mapping file
 #'
-#' This function constructs a CASE WHEN THEN statement from a mapping file. 
-#' It assumes that the first column of the mapping file contains the original
-#' WHEN values, and the second column contains the THEN values (the values
-#' to be mapped to.)
+#' This function constructs a CASE WHEN THEN statement from a mapping file or
+#' dataframe. It assumes that the first column of the mapping data contains the
+#' original WHEN values, and the second column contains the THEN values (the
+#' values to be mapped to.)
 #'
-#' @param inputfile Path to the mapping file
+#' @param inputfile Mapping dataframe OR path to the mapping file
 #' @return A string that represents the constructed CASE statement
 #' @export
 casewhen <- function(inputfile){
-  mapping <- read.csv(inputfile)
+  if (class(inputfile)=="character") {
+    mapping <- read.csv(inputfile)
+  } else {
+    mapping <- inputfile
+  }
   statement <- "\nCASE"
   for (i in 1:nrow(mapping)){
     statement = paste(statement, " WHEN ", "'", mapping[i, 1],
