408606558df826b5b658b889e422d76f1272c484
Leoson Hoay <leosonhoay@gmail.com>
2022-12-22 11:16:13 -0600

    v0.1.2

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,10 @@
 # *News*
 
+# sqlcaseR 0.1.2 (2022-12-22)
+
+* Added header argument to specify if input file has a header row.
+* Added error message for unspecified inputs.
+
 # sqlcaseR 0.1.1 (2022-12-20)
 
 * Added functionality to read in dataframes in addition to file paths.
--- R/casewhen.R
+++ R/casewhen.R
@@ -6,11 +6,16 @@
 #' values to be mapped to.)
 #'
 #' @param inputfile Mapping dataframe OR path to the mapping file
+#' @param header If reading a csv file, TRUE if the file includes a header row,
+#' FALSE if it does not include a header row.
 #' @return A string that represents the constructed CASE statement
 #' @export
-casewhen <- function(inputfile){
+casewhen <- function(inputfile = NULL, header = FALSE){
+  if (is.null(inputfile) == TRUE) {
+    stop("Please include a file path or an input dataframe.")
+  }
   if (class(inputfile)=="character") {
-    mapping <- read.csv(inputfile)
+    mapping <- read.csv(inputfile, header = header)
   } else {
     mapping <- inputfile
   }
